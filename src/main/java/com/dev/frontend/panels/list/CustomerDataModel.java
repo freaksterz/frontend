8a407ecc106bfed7d310e899000bb0f38eae029a
aman gupta <aman_gupta2@3dplmsoftware.com>
2015-02-05 20:09:55 +0530

    added List Services

--- src/main/java/com/dev/frontend/panels/list/CustomerDataModel.java
+++ src/main/java/com/dev/frontend/panels/list/CustomerDataModel.java
@@ -1,5 +1,6 @@
 package com.dev.frontend.panels.list;
 
+import java.util.Arrays;
 import java.util.List;
 
 import com.dev.frontend.services.Services;
@@ -18,15 +19,16 @@ public class CustomerDataModel extends ListDataModel
 	{
 		return Services.TYPE_CUSTOMER;
 	}
-
+	/*
+	* This method use list returned by Services.listCurrentRecords and should convert it to array of rows
+	* each row is another array of columns of the row
+		 */
 	@Override
 	public String[][] convertRecordsListToTableModel(List<Object> list)
 	{
-		//TODO by the candidate
-		/*
-		 * This method use list returned by Services.listCurrentRecords and should convert it to array of rows
-		 * each row is another array of columns of the row
-		 */
+		Object[] objectArray = list.toArray();
+		String[] stringArray = Arrays.copyOf(objectArray, objectArray.length, String[].class);
+
 		String[][] sampleData = new String [][]{{"01","Customer 1","+201011121314","23.4"},{"02","Customer 2","+201112131415","1.4"}};
 		return sampleData;
 	}
